65e3419505df17158b34d3068fb1698c816fe47d
Ankit-0009 <ankitcode2511@gmail.com>
2024-09-16 15:25:30 +0530

    Create the database structure in appwrite by implementing relations with differnt entities and built a structure. Also implement the backend logic of add a user in database when he sign-up.

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,7 @@
 import { INewUser } from "../../types";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, database } from "./config";
+
 export async function createUserAccount (user: INewUser) {
     try {
         const newAccount = await account.create(
@@ -10,9 +11,44 @@ export async function createUserAccount (user: INewUser) {
             user.name
         );
 
-        return newAccount;
+        // Now we want to add new user to our database.
+        if(!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            email: newAccount.email,
+            name: newAccount.name,
+            username: user.username,
+            imageUrl: avatarUrl
+        });
+
+        return newUser;
     } catch (error) {
         console.log(error);
         return error;
+    }
+}
+
+// Creating saveUserToDB function.
+export async function saveUserToDB(user : {
+    accountId: string,
+    email: string,
+    name: string,
+    imageUrl: URL,
+    username?: string,
+}) {
+    try {
+        const newUser = await database.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        )
+
+        return newUser;
+    } catch (error) {
+        console.log(error);        
     }
 }
